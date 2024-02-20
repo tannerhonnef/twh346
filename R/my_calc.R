0d8011ab6c0e7b258ca3d2986386b79e864b5a63
tannerhonnef <thonnef@clarku.edu>
2024-02-20 01:14:22 +0000

    update with assignment 2

---
+++ R/my_calc.R
@@ -0,0 +1,26 @@
+#' A calculator function
+#' @description This function is a calculator that takes vectors and matrices
+#' # as input
+#' @param x A number, vector, or matrix
+#' @param y A number, vector, or matrix
+#' @param fun A string with an operation to be performed: sum, subtract,
+#' divide, multiply
+#' @export
+#' @examples
+#' my_calc(7, 5, "sum")
+#' my_calc(10:15, 1:5, "divide")
+#'  my_calc(2, 5, "multiply")
+my_calc <- function(x, y, fun) {
+if(fun == "sum") {
+  o <- x + y
+} else if(fun == "subtract") {
+  o <- x - y
+} else if(fun == "divide") {
+  o <- x / y
+} else if(fun == "multiply") {
+  o <- x * y
+} else {
+  o <- paste("Not a recognized function")
+}
+return(o)
+}
